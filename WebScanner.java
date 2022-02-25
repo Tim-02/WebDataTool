cb39d8d6413d55eea88dd7f63fb2aee4f2913357
Tim <95551534+Tim-02@users.noreply.github.com>
2022-02-25 17:47:23 -0500

    Update WebScanner.java
    added getData data

--- WebScanner.java
+++ WebScanner.java
@@ -42,9 +42,44 @@ public class WebScanner {
             }
         }
         
-        public static String[] getData(String url){
-          // to be added  
-          return null;
+         public static String[] getData(String url){
+            String[] data = new String[10];
+            try{
+                Elements e1;
+                Document doc = Jsoup.connect(url).get();  
+                // 0: name
+                String temp = url.replace("https://www.livecoinwatch.com/price/", "");
+                data[0] = temp.substring(0, temp.indexOf('-'));
+                // 1: sign
+                data[1] = temp.substring(temp.indexOf('-')+1);
+                // 2: value
+                e1 = doc.select("[class=cion-item coin-price-large]");
+                data[2] = e1.select("[class=price]").first().text().replace("$","");
+                // 3: market cap
+                e1 = doc.select("[class=cion-item text-left text-sm-center text-lg-left first-row-col]");
+                data[3] = e1.select("span").text().replace("$","");
+                // 4: volume
+                e1 = doc.select("[class=cion-item text-center text-lg-left first-row-col]");
+                data[4] = e1.select("span").text().replace("$","");                
+                // 5: circulation
+                e1 = doc.select("[class=cion-item col-md-4 col-lg-3 px-1 py-1 py-md-0 text-left text-sm-center text-md-right]");
+                data[5] = e1.select("span").text();  
+                // 6: total supply 
+                e1 = doc.select("[class=cion-item col-md-4 col-lg-3 px-1 py-1 py-md-0 text-center text-md-right]");
+                data[6] = e1.select("span").text(); 
+                // 7: liquidity
+                e1 = doc.select("[class=cion-item col-md-4 col-xl-3 px-1 py-1 py-md-0 text-left text-sm-center text-md-right]");
+                data[7] = e1.select("span").get(1).text();    
+                // 8: Bids
+                e1 = doc.select("[class=cion-item px-1 py-1 py-md-0 text-center col-md-4 col-xl-3 text-md-right]");
+                data[8] = e1.select("span").text();    
+                // 9: Asks
+                e1 = doc.select("[class=cion-item px-1 py-1 py-md-0 text-right text-sm-center col-md-4 col-xl-3 text-md-right]");
+                data[9] = e1.select("span").text();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return data;
         }
     }
 }
